20cb8a2f872c6042699cebfb116630734da0d05e
Abdelrahman Nasser <abdelrahmanhydara@gmail.com>
2024-06-26 22:03:27 +0300

    - added development & productions flavors for android - added launch.json for flavors - added two main files for each flavor - changed android app name for each flavor

---
+++ lib/main_production.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'core/di/dependency_injection.dart';
+import 'docdoc.dart';
+
+void main() async{
+  setupGetIt();
+  /// To fix texts being hidden bug in flutter_screenutil in release mode
+  await ScreenUtil.ensureScreenSize();
+  runApp(const DocDocApp());
+}
+
